1402fde469341efafc4fee2e42210e4d9be57559
Bharat9098 <kha.bharat9098@gmail.com>
2025-04-08 15:00:00 +0530

    Proper naming

--- Arrays/array-size.rb
+++ Arrays/array-size.rb
@@ -1,2 +1,8 @@
+git config user.name "Bharat9098"
+git config user.email "kha.bharat9098@gmail.com"
+
+GIT_AUTHOR_DATE="2025-04-08 15:00:00 +0530" \
+GIT_COMMITTER_DATE="2025-04-08 15:00:00 +0530" \
+git commit --allow-empty -m "test commit"
 
 users.size # 3
