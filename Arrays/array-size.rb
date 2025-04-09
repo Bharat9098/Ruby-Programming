a70622cc52f21050428caa8212e55993854d0da7
Bharat9098 <kha.bharat9098@gmail.com>
2025-04-09 16:00:00 +0530

    fix namings

--- Arrays/array-size.rb
+++ Arrays/array-size.rb
@@ -3,6 +3,6 @@ git config user.email "kha.bharat9098@gmail.com"
 
 GIT_AUTHOR_DATE="2025-04-08 15:00:00 +0530" \
 GIT_COMMITTER_DATE="2025-04-08 15:00:00 +0530" \
-git commit --allow-empty -m "test commit"
+git commit --allow-empty -m "Proper naming"
 
 users.size # 3
