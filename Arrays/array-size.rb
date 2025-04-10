97280da65b43c62aa26ff2431a8edef41e91bafb
Bharat9098 <kha.bharat9098@gmail.com>
2025-04-10 17:00:00 +0530

    Some updates to files

--- Arrays/array-size.rb
+++ Arrays/array-size.rb
@@ -1,8 +1,8 @@
 git config user.name "Bharat9098"
 git config user.email "kha.bharat9098@gmail.com"
 
-GIT_AUTHOR_DATE="2025-04-08 15:00:00 +0530" \
-GIT_COMMITTER_DATE="2025-04-08 15:00:00 +0530" \
-git commit --allow-empty -m "Proper naming"
+GIT_AUTHOR_DATE="2025-04-09 16:00:00 +0530" \
+GIT_COMMITTER_DATE="2025-04-09 16:00:00 +0530" \
+git commit --allow-empty -m "fix namings"
 
 users.size # 3
