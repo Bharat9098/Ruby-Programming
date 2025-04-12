d06845f0965618d2478dd36823e708109abca2f0
Bharat9098 <kha.bharat9098@gmail.com>
2025-04-12 14:00:00 +0530

    remove un-needed files

--- Arrays/array-size.rb
+++ Arrays/array-size.rb
@@ -1,8 +1,8 @@
 git config user.name "Bharat9098"
 git config user.email "kha.bharat9098@gmail.com"
 
-GIT_AUTHOR_DATE="2025-04-09 16:00:00 +0530" \
-GIT_COMMITTER_DATE="2025-04-09 16:00:00 +0530" \
-git commit --allow-empty -m "fix namings"
+GIT_AUTHOR_DATE="2025-04-11 17:00:00 +0530" \
+GIT_COMMITTER_DATE="2025-04-11 17:00:00 +0530" \
+git commit --allow-empty -m "merge and remove duplicate"
 
 users.size # 3
